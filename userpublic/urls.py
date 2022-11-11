cc69537bb2d9c05fd8b87f9e879ad84ffa592e1d
samuel abasah <kobysam13@gmail.com>
2022-11-11 09:57:25 -0500

    first commit

--- userpublic/urls.py
+++ userpublic/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from userpublic.views import HomePageView
 
 urlpatterns = [
-    path('home/', HomePageView.as_view(), name='homepage' ),
+    path('', HomePageView.as_view(), name='homepage' ),
+    #path('', RedirectView.as_view(url='/home/')),
 ]
